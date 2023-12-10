c047249ad8fd31fead315e788f56b41b57ca388e
linxi0908 <mylinh.phan.water@gmail.com>
2023-12-10 10:34:16 +0700

    update 1

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,69 @@
+$(document).ready(function () {
+    var cartData = localStorage.getItem("cart");
+    var cartItems = cartData ? JSON.parse(cartData) : [];
 
+    $(".add-to-cart").each(function () {
+        var productId = $(this).data("product-id");
+
+        if ($.inArray(productId, cartItems) !== -1) {
+            disableAddToCartButton($(this));
+        }
+    });
+
+    $(".add-to-cart").on("click", function (event) {
+        event.preventDefault();
+        var url = $(this).data("url");
+        var addButton = $(this);
+        var productId = $(this).data("product-id");
+
+        if ($.inArray(productId, cartItems) !== -1) {
+            return;
+        }
+
+        $.ajax({
+            method: "get",
+            url: url,
+            data: {
+                cart: localStorage.getItem("cart"),
+            },
+            success: function (response) {
+                localStorage.setItem("cart", JSON.stringify(response.cart));
+                disableAddToCartButton(addButton);
+                $("#cart-total").html(number_format(response.total_price, 2));
+            },
+        });
+    });
+
+    function disableAddToCartButton(button) {
+        button.html(
+            '<img src="https://raw.githubusercontent.com/danny-nguyen-goldenowl/webdev-intern-assignment/main/app/assets/check.png" alt="Added">'
+        );
+        button.prop("disabled", true);
+    }
+
+    $(".icon_close").on("click", function () {
+        var url = $(this).data("url");
+        var productId = $(this).data("id");
+        $.ajax({
+            method: "GET",
+            url: url,
+            data: {
+                _token: "{{ csrf_token() }}",
+            },
+            success: function (response) {
+                if (response.success) {
+                    var cartData = localStorage.getItem("cart");
+                    var cartItems = cartData ? JSON.parse(cartData) : [];
+                    if (cartItems.hasOwnProperty(productId)) {
+                        delete cartItems[productId];
+                        localStorage.setItem("cart", JSON.stringify(cartItems));
+                        $("#cart-total").html(
+                            formatNumber(response.total_price)
+                        );
+                    }
+                    $("#" + productId).empty();
+                }
+            },
+        });
+    });
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,7 +55,7 @@
                         <img src="https://raw.githubusercontent.com/danny-nguyen-goldenowl/webdev-intern-assignment/main/app/assets/nike.png" alt="nike">
                         <div class="card-header">
                             <h3>Your cart</h3>
-                            <h3 id="cart-total">$ {{ number_format( $total_price, 2) }}</h3>
+                            <h3 id="cart-total">$ 0.00</h3>
                         </div>
                         <div class="card-cart">
                             @if (!empty($cart))
@@ -114,76 +114,8 @@
     {{-- JS --}}
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="{{ asset('assets/js/main.js') }}"></script>
-    <script>
-        $(document).ready(function() {
-            var cartData = localStorage.getItem('cart');
-            var cartItems = cartData ? JSON.parse(cartData) : [];
-
-            $('.add-to-cart').each(function() {
-                var productId = $(this).data('product-id');
-
-                if ($.inArray(productId, cartItems) !== -1) {
-                    disableAddToCartButton($(this));
-                }
-            });
-
-            $('.add-to-cart').on('click', function(event) {
-                event.preventDefault();
-                var url = $(this).data('url');
-                var addButton = $(this);
-                var productId = $(this).data('product-id');
-
-                if ($.inArray(productId, cartItems) !== -1) {
-                    return;
-                }
-
-                $.ajax({
-                    method: 'get'
-                    , url: url
-                    , data: {
-                        cart: localStorage.getItem('cart')
-                    }
-                    , success: function(response) {
-                        localStorage.setItem('cart', JSON.stringify(response.cart));
-                        disableAddToCartButton(addButton);
-                        $('#cart-total').html(number_format(response.total_price,2));
-                    }
-                , });
-            });
-
-            function disableAddToCartButton(button) {
-                button.html('<img src="https://raw.githubusercontent.com/danny-nguyen-goldenowl/webdev-intern-assignment/main/app/assets/check.png" alt="Added">');
-                button.prop('disabled', true);
-            }
-
-            $('.icon_close').on('click', function() {
-                var url = $(this).data('url');
-                var productId = $(this).data('id');
-                $.ajax({
-                    method: 'GET'
-                    , url: url
-                    , data: {
-                        '_token': '{{ csrf_token() }}'
-                    }
-                    , success: function(response) {
-                        if (response.success) {
-                            var cartData = localStorage.getItem('cart');
-                            var cartItems = cartData ? JSON.parse(cartData) : [];
-                            if (cartItems.hasOwnProperty(productId)) {
-                                delete cartItems[productId];
-                                localStorage.setItem('cart', JSON.stringify(cartItems));
-                                $('#cart-total').html(formatNumber(response.total_price));
-                            }
-                            $('#' + productId).empty();
-                        }
-
-                    }
-                });
-            });
-
-    </script>
 </body>
 </html>
